9b876d773a7bda3203752925261b92c1b2e71010
Razvan Popa <razvancristianpopa@gmail.com>
2018-08-21 21:03:30 +0300

    First Component
    I add my first component and show to the homepage.

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Vladimir Kliscoci</h4>
+        <ul>
+            <li>Email: putin@gmail.com</li>
+            <li>Phone: 800-900-1000</li>
+        </ul>
+      </div>
+    )
+  }
+}
+export default Contact;
