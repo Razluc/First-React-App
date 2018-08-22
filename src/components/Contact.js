8b178e1911b60897600da922d8ad1ede3a6a7797
Razvan Popa <razvancristianpopa@gmail.com>
2018-08-22 23:17:52 +0300

    Add font awesome and change state on click
    On click to the icon you can change the state.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
   render() {
     const { name, email, phone } = this.props.contact; //the best way. Insted of this you need to add on each place this.props.name for example.
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.defaultProps = {
-  name: 'Dumitru',
-  email: 'max@imo.no',
-  phone: '123-123-123'
+  name: "Dumitru",
+  email: "max@imo.no",
+  phone: "123-123-123"
 };
 Contact.PropTypes = {
   contact: PropTypes.object.isRequired
