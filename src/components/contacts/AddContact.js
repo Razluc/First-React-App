09dbab00b0bf8ba1d357e1d0821d2adf966fc833
Razvan Popa <razvancristianpopa@gmail.com>
2018-08-29 20:55:21 +0300

    Form fix
    Now type is possible in the form. And when click on submit button the state is visible in console.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,18 @@ class AddContact extends Component {
     email: "",
     phone: ""
   };
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name </label>
               <input
@@ -21,6 +26,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Name..."
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -31,6 +37,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter email..."
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -41,6 +48,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter phone..."
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
